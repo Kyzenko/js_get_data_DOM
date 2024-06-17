5e2f5d86fa668a575c651050db90422b43436511
Nazarii <Kyzenko.nr@gmail.com>
2024-06-17 10:24:14 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+// write your code here
+const populationCountries = [
+    ...document.querySelectorAll(".population")
+];
+const totalPopulation = document.querySelector(".total-population");
+const averagePopulation = document.querySelector(".average-population");
+const total = populationCountries.reduce((prev, current)=>{
+    const populationCountry = +current.textContent.replaceAll(",", "");
+    return prev + populationCountry;
+}, 0);
+const average = total / populationCountries.length;
+totalPopulation.textContent = total.toLocaleString();
+averagePopulation.textContent = average.toLocaleString();
+
+//# sourceMappingURL=index.f75de5e1.js.map
